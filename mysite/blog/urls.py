d55d3217cdb18089aa40455293b25e4847551834
Nakib Momin <nbmomin14@gmail.com>
2015-04-05 18:29:18 -0400

    Added index view

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.Index, name="index"),
+  
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,11 @@
+from django.http import Http404
 from django.shortcuts import render
+from . import models
+
+def Index(request):
+	posts = models.PostEntry.objects.published().order_by('-created')
+	context = {'posts': posts}
+	return  render(request, 'blog/index.html', context)
+	
 
 # Create your views here.
